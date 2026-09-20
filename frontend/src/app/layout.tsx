cbe948f7527d0622dd96d86834560188d6c9fb03
dhyey9610116003 <dhyey.raja153148@marwadiuniversity.ac.in>
2026-09-20 18:14:25 +0530

    Fix AdSense verification by using raw script tag and adding ads.txt

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -100,12 +100,7 @@ export default function RootLayout({
             ])
           }}
         />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3444542685708016"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3444542685708016" crossOrigin="anonymous"></script>
       </head>
       <body className="min-h-full flex flex-col">
         <GoogleOAuthProvider clientId="234819018700-s05ud8ua2h7eqp9t99jhm8ki6sqircjn.apps.googleusercontent.com">
